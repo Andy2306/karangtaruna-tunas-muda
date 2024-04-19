a273706fa985c95a3d9f1258960e071b8411fad5
andy <72691938+Andy2306@users.noreply.github.com>
2024-04-19 02:26:35 +0000

    update

--- source/script.js
+++ source/script.js
@@ -1,3 +1,25 @@
+//gallery slideshow
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideG");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
+
+//slideshow
 var myIndex = 0;
 carousel();
 
@@ -12,3 +34,35 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+// animasi scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click',function (e){
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      })
+  })
+})
+
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const FixedNav = header.offsetTop;
+
+  if (window.pageYOffset > FixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Navbar button
+const navbutton = document.querySelector("#navbutton");
+const navMenu = document.querySelector('#nav-menu');
+
+navbutton.addEventListener("click", function () {
+  navbutton.classList.toggle("button-active");
+  navMenu.classList.toggle('hidden');
+});
