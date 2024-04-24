a45717804b29fae418e6e89a3c5e5928254710a1
andy <72691938+Andy2306@users.noreply.github.com>
2024-04-24 04:06:13 +0000

    update

--- source/script.js
+++ source/script.js
@@ -47,3 +47,53 @@ navbutton.addEventListener("click", function () {
   navbutton.classList.toggle("button-active");
   navMenu.classList.toggle('hidden');
 });
+
+//animasi masuk
+/*window.addEventListener("scroll", function () {
+  const box = document.querySelector("#animateSlide");
+  const distanceFromTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (distanceFromTop < windowHeight / 2) {
+    box.classList.add("box");
+  } else {
+    box.classList.remove("box");
+  }
+});
+*/
+
+// Mendapatkan referensi elemen yang ingin diubah
+var elemen = document.getElementById('animateSlide');
+var elemensatu = document.getElementById('animateSlideSatu');
+var elemendua = document.getElementById('animateSlideDua')
+
+// Fungsi untuk menambahkan atau menghilangkan class
+function toggleClassOnScroll() {
+  var lastScrollTop = 0; // Variabel untuk menyimpan posisi scroll terakhir
+
+  window.addEventListener('scroll', function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    // Jika scroll ke bawah dan melewati kelipatan 400px, tambahkan class
+    if (currentScroll > lastScrollTop && currentScroll % 400 < 100) {
+      elemen.classList.add('box');
+    }
+    else if (currentScroll > lastScrollTop && currentScroll % 425 < 100) {
+      elemensatu.classList.add('box');
+    }
+    else if (currentScroll > lastScrollTop && currentScroll % 775 < 100) {
+      elemendua.classList.add('box')
+    }
+    // Jika scroll ke atas, hilangkan class
+    else if (currentScroll < lastScrollTop && currentScroll % 400 > 300) {
+      elemen.classList.remove('box');
+      elemensatu.classList.remove('box');
+      elemendua.classList.remove('box')
+    }
+    
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Perbarui posisi scroll terakhir
+  }, false);
+}
+
+// Memanggil fungsi saat dokumen siap
+document.addEventListener('DOMContentLoaded', toggleClassOnScroll);
